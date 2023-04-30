c12332f58f9f3ee0614d7fdcc69c0a4a40a6660a
Brink2Three <drewisbrink@gmail.com>
2023-04-30 16:21:48 -0400

    Updated logic in mtaled.py to pull all train lines in order

--- mtaled.py
+++ mtaled.py
@@ -10,29 +10,37 @@ load_dotenv()
 
 MTA_API_KEY = os.getenv('api_key')
 
-pixels1 = neopixel.NeoPixel(board.D18, 180, brightness=.15)
+#pixels1 = neopixel.NeoPixel(board.D18, 180, brightness=.15)
 
 x=0
 
 # Line colors so I don't have to type the codes out
 line_123 = (127, 0, 0)
 line_456 = (0, 127, 7)
-line_ace = (6, 20, 127)
 line_7 = (100, 0, 100)
+line_ace = (6, 20, 127)
 line_bdfm = (255, 107, 0)
 line_jz = (190, 50, 14)
 line_nqrw = (252, 204, 10)
 line_ls = (128, 129, 131)
 bg = (5, 5, 5)
 
+strings = ["1", "2", "3", "4", "5", "6", "7", "A", "C", "E", "B", "D", "F", "M", "N", "Q", "R", "W", "J", "Z", "L", "S"]
 
-line_id = "1"
+def process_string(string):
+    # do some processing on the string
+    processed_string = string
+    return processed_string
 
+for string in strings:
+    line_id = process_string(string)
 
 def main():
-    line = [line_id]
-    feed = NYCTFeed(line_id, api_key=MTA_API_KEY)
-    print_feed(feed, line)
+    for string in strings:
+        line_id = process_string(string)
+        line = [line_id]
+        feed = NYCTFeed(line_id, api_key=MTA_API_KEY)
+        print_feed(feed, line)
 
 #Trying to extrapolate data so I can feed the stopped train's stations into a variable. 
 def print_feed(feed, line_id):
