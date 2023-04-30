28fd6460707cafccd6bac1f32bd39e8fcdea4a36
Brink2Three <drewisbrink@gmail.com>
2023-04-29 21:34:24 -0400

    Add pipfile

--- MTA-api-requests-test v2.py
+++ MTA-api-requests-test v2.py
@@ -1,7 +1,15 @@
+import time
+#import board
+#import neopixel
 import os
 from datetime import datetime, timedelta
-
 from nyct_gtfs import NYCTFeed
+from dotenv import load_dotenv
+load_dotenv()
+
+MTA_API_KEY = os.getenv('api_key')
+
+
 
 def print_feed(feed, line_id, direction):
     for trip in feed.filter_trips(line_id, direction):
@@ -9,7 +17,7 @@ def print_feed(feed, line_id, direction):
 
 def main():
     line = ["1"]
-    feed = NYCTFeed("1", api_key='API KEY HERE')
+    feed = NYCTFeed("1", api_key=MTA_API_KEY)
     print_feed(feed, line, "N")
     print_feed(feed, line, "S")
 
